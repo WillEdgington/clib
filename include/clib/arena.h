8942f2fc43bd8b040ee2ab8e00d3b9229745ee23
WillEdgington <willedge037@gmail.com>
2026-04-27 19:05:56 +0100

    feat: implement chained arena allocator

--- include/clib/arena.h
+++ include/clib/arena.h
@@ -1,1 +1,24 @@
-/* Placeholder */
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <stddef.h>
+
+typedef struct Region {
+  char *buffer;
+  size_t capacity;
+  size_t offset;
+  struct Region *next;
+} Region;
+
+typedef struct {
+  Region *head;
+  Region *current;
+  size_t slab_size;
+} Arena;
+
+int arena_init(Arena *a, size_t slab_size);
+void *arena_alloc(Arena *a, size_t size);
+int arena_reset(Arena *a);
+void arena_free(Arena *a);
+
+#endif
--- src/arena.c
+++ src/arena.c
@@ -1,1 +1,64 @@
-/* Placeholder */
+#include "../include/clib/arena.h"
+
+#include <stdlib.h>
+
+static Region *region_create(size_t capacity) {
+  Region *r = malloc(sizeof(Region) + capacity);
+  if (r == NULL)
+    return NULL;
+  r->capacity = capacity;
+  r->buffer = (char *)(r + 1); // point to memory right after struct
+  r->offset = 0;
+  r->next = NULL;
+  return r;
+}
+
+int arena_init(Arena *a, size_t slab_size) {
+  a->slab_size = slab_size;
+  return 0;
+}
+
+void *arena_alloc(Arena *a, size_t size) {
+  size = (size + 7) & ~7; // align requested size to 8 bytes
+  if (a->head == NULL) {
+    a->head = region_create(a->slab_size < size ? size : a->slab_size);
+    if (a->head == NULL)
+      return NULL;
+    a->current = a->head;
+  }
+
+  while (a->current->capacity - a->current->offset < size) {
+    if (a->current->next == NULL) {
+      size_t capacity = a->slab_size < size ? size : a->slab_size;
+      Region *new_r = region_create(capacity);
+      if (new_r == NULL)
+        return NULL;
+      a->current->next = new_r;
+    }
+    a->current = a->current->next;
+    a->current->offset = 0; // reset offset for case of old region
+  }
+
+  void *ptr = a->current->buffer + a->current->offset;
+  a->current->offset += size;
+  return ptr;
+}
+
+int arena_reset(Arena *a) {
+  if (a->head == NULL)
+    return -1;
+  a->current = a->head;
+  a->current->offset = 0;
+  return 0;
+}
+
+void arena_free(Arena *a) {
+  Region *node = a->head;
+  while (node != NULL) {
+    Region *next_node = node->next;
+    free(node);
+    node = next_node;
+  }
+  a->head = NULL;
+  a->current = NULL;
+}

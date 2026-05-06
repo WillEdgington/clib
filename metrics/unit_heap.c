9908dedc509b485d7fbd04d8ac17656308e26f2f
WillEdgington <willedge037@gmail.com>
2026-05-06 18:33:52 +0100

    feat: (metrics) benchmark heapify mechanism

--- metrics/unit_heap.c
+++ metrics/unit_heap.c
@@ -1,5 +1,6 @@
 #include "clib/heap.h"
 #include "clib/metric_framework.h"
+#include "clib/vector.h"
 
 #define N_OPERATIONS 1000000
 
@@ -10,9 +11,7 @@ static void env_info() {
   printf("==========================================\n\n");
 }
 
-int main() {
-  printf("\nRunning: %s...\n", __FILE__);
-  env_info();
+static void run_basic_heap_ops() {
   Heap h;
   heap_init(&h, sizeof(int), NULL);
 
@@ -39,5 +38,24 @@ int main() {
   });
 
   heap_free(&h);
+}
+
+static void run_heapify() {
+  Vector v;
+  Heap h;
+  vector_init(&v, sizeof(int));
+  heap_init(&h, sizeof(int), NULL);
+  for (int i = N_OPERATIONS; i >= 0; i--)
+    vector_push(&v, &i);
+  BENCHMARK_FULL("Heapify Vector (1M Descending Values)", 1,
+                 { heap_heapify(&h, &v); });
+  heap_free(&h);
+}
+
+int main() {
+  printf("\nRunning: %s...\n", __FILE__);
+  env_info();
+  run_basic_heap_ops();
+  run_heapify();
   return 0;
 }
